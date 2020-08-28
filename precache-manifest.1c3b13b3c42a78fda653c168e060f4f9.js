d83329a45defce27b86336dd9849c651b39e7bd6
Luis Rosales <eng.luisrosales@gmail.com>
2020-08-28 13:34:55 +0200

    Updates

---
+++ precache-manifest.1c3b13b3c42a78fda653c168e060f4f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e88a2a1af8f4bb2078dead88780909f",
+    "url": "/sendbird-poc/index.html"
+  },
+  {
+    "revision": "503bac565230c88e9095",
+    "url": "/sendbird-poc/static/css/main.5995eb13.chunk.css"
+  },
+  {
+    "revision": "ba8d0bdcad8b06ecd19c",
+    "url": "/sendbird-poc/static/js/2.ef6ac467.chunk.js"
+  },
+  {
+    "revision": "e8b0ae1bd2f59a85f300b3c60c50941a",
+    "url": "/sendbird-poc/static/js/2.ef6ac467.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "503bac565230c88e9095",
+    "url": "/sendbird-poc/static/js/main.d19058b2.chunk.js"
+  },
+  {
+    "revision": "d886f03b63109d196f4e",
+    "url": "/sendbird-poc/static/js/runtime-main.73f53162.js"
+  }
+]);
